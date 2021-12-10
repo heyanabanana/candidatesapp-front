f023e8b4828a4afe603b0378fcfe44ffb64054bf
Ana Fernández Valdés <91973567+heyanabanana@users.noreply.github.com>
2021-12-10 18:43:46 +0100

    build fix problems

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useLocation, Link } from "wouter";
 import useUser from "../config/useUser";
 import { useForm } from "react-hook-form";
